d5ba5eae6f99309d42b63dcc7d75a21e0b879e9c
Angel De La Cruz <angeldelacruzdev@gmail.com>
2022-05-30 08:53:35 -1200

    Tailwind Crash Course | Project From Scratch

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
